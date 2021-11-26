39f2d5dd41eac0dd200029cb9879c038fd3d452b
Arnav1121 <82814114+Arnav1121@users.noreply.github.com>
2021-11-26 15:51:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+var mouseEvent = "";
+
+var last_x,last_y;
+
+var color = "blue";
+
+var linewidth = 2;
+
+canvas = document.getElementById("myCanvas");
+
+ctx=canvas.getContext("2d");
+
+canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e){
+
+mouseEvent="mousedown";
+
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+
+function my_mouseleave(e){
+
+mouseEvent="mouseleave";
+
+}
+
+canvas.addEventListener("mouseup",my_mouseup);
+
+function my_mouseup(e){
+
+mouseEvent="mouseup";
+
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+
+function my_mousemove(e){
+
+    current_x=e.clientX-canvas.offsetLeft;
+
+    current_y=e.clientY-canvas.offsetTop;
+
+if(mouseEvent=="mousedown"){
+
+ctx.beginPath();
+
+ctx.strokeStyle=color;
+
+ctx.lineWidth=linewidth;
+
+ctx.arc(current_x,current_y,20,0,2*Math.PI);
+
+ctx.stroke();
+
+}
+
+last_x=current_x;
+
+last_y=current_y;
+
+
+
+}
